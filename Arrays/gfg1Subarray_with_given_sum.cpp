801400088e5eb02df10c35b5c99b0f537519b746
Nisha Jadhav <57458313+Nisha1607@users.noreply.github.com>
2021-10-06 19:57:07 +0530

    Update gfg1Subarray_with_given_sum.cpp

--- Arrays/gfg1Subarray_with_given_sum.cpp
+++ Arrays/gfg1Subarray_with_given_sum.cpp
@@ -1,4 +1,14 @@
- vector<int> subarraySum(int arr[], int n, int s)
+Q 1 .Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-array which adds to a given number S .
+ 
+ Input:
+N = 5, S = 12
+A[] = {1,2,3,7,5}
+Output: 2 4
+Explanation: The sum of elements 
+from 2nd position to 4th position 
+is 12.
+
+vector<int> subarraySum(int arr[], int n, int s)
     {
         vector<int> v;
         int sum_till_now=arr[0];
